019a82b65c2c0ffc7f9f134d269239f70a05cb10
mark sauer-utley <mark.utley.713@gmail.com>
2018-07-17 07:41:49 -0400

    start form for character creation

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,54 @@
+class CharactersController < ApplicationController
+
+  def index
+    @characters = Character.all
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    args = [:user_id, :name, :job_id, :race_id, :magic_school_id, :subclass_id, :subrace_id, :level_id]
+    @character = Character.new(character_params(*args))
+    byebug
+    if @character.save
+      redirect_to character_path(@character)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_character
+    @character = Character.find(params[:id])
+  end
+
+  def character_params(*args)
+    params.require(:character).permit(*args)
+  end
+
+end
+
+
+# "user_id"
+# "race_id"
+# "job_id"
+#  "name"
+#  "magic_school_id"
+#  "subclass_id"
+#  "subrace_id"
+#  "level"
